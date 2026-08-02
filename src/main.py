2e04dfe8a0b748303fda4995b4f49ab4ae6d16ff
Mathetes <260054114+mathetes2602@users.noreply.github.com>
2026-08-02 14:43:34 +0300

    copy files from static to public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
-from textnode import TextType, TextNode
+import os
+import shutil
 
+def copy_files(origin, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    os.mkdir(destination)
+    dir_items = os.listdir(origin)
+    for item in dir_items:
+        item_path = os.path.join(origin, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, os.path.join(destination, item))
+        if os.path.isdir(item_path):
+            copy_files(item_path, os.path.join(destination, item))
+    
 def main():
-    node = TextNode("hello", TextType.PLAIN)
-    print(node) 
-
+    main_dir = os.path.abspath("./")
+    public_path = os.path.join(main_dir, "public")
+    static_path = os.path.join(main_dir, "static")
+    copy_files(static_path, public_path)      
 main()
